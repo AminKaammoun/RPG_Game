39dadf9bccb1314bf0dbee6c3e38a2f92e39fa3a
AminKaammoun <aminkammoun.2121@gmail.com>
2022-08-06 22:50:51 +0100

    SetUp the base for toolTip system

--- Assets/Script/toolTip.cs
+++ Assets/Script/toolTip.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class toolTip : MonoBehaviour
+
 {
+
+    public RectTransform rectTransform;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 position = Input.mousePosition;
+        //float pivotX = position.x / Screen.width;
+        //float pivotY = position.y / Screen.height;
+        //rectTransform.pivot = new Vector2(pivotX, pivotY);
         transform.position = position;
     }
 }
--- Assets/Script/toolTipTrigger.cs
+++ Assets/Script/toolTipTrigger.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class toolTipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    private bool Show;
     public void OnPointerEnter(PointerEventData eventData)
     {
-        toolTipSystem.show();
+        Show = true;
+        StartCoroutine(startShow());
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         toolTipSystem.hide();
+        Show = false;
     }
 
-    
-}
+    IEnumerator startShow()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (Show)
+        {
+            toolTipSystem.show();
+        }
+    }
+
+} 
