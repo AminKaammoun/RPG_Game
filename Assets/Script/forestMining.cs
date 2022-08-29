d65ac5a3c53f9b10c96133a39045e1c8cf7350f0
AminKaammoun <aminkammoun.2121@gmail.com>
2022-08-29 23:57:09 +0100

    Make stones, trees and gemStones spawn every 4 hours

--- Assets/Script/forestMining.cs
+++ Assets/Script/forestMining.cs
@@ -16,7 +16,7 @@ public class forestMining : MonoBehaviour
     public AudioClip forestNightAudio;
 
     public GameObject[] rockSpawnAreas;
-    public GameObject[] gemStoneSpawnArea;
+    public GameObject[] gemStoneSpawnAreas;
     public GameObject rock;
     public GameObject blueGemStone;
     public GameObject redGemStone;
@@ -24,14 +24,15 @@ public class forestMining : MonoBehaviour
     public GameObject yellowGemStone;
     public GameObject orangeGemStone;
 
+    private bool spawn = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        gemStoneSpawnArea = GameObject.FindGameObjectsWithTag("gemStoneSpawnArea");
+        gemStoneSpawnAreas = GameObject.FindGameObjectsWithTag("gemStoneSpawnArea");
         rockSpawnAreas = GameObject.FindGameObjectsWithTag("rockSpawnArea");
-        
-        foreach(GameObject rockSpawnArea in rockSpawnAreas)
+
+        foreach (GameObject rockSpawnArea in rockSpawnAreas)
         {
             int rand = Random.Range(0, 4);
             switch (rand)
@@ -42,9 +43,11 @@ public class forestMining : MonoBehaviour
             }
         }
 
-        foreach (GameObject gemStoneSpawnArea in gemStoneSpawnArea)
+        foreach (GameObject gemStoneSpawnArea in gemStoneSpawnAreas)
         {
+
             int rand = Random.Range(0, 20);
+
             switch (rand)
             {
                 case 0:
@@ -62,6 +65,7 @@ public class forestMining : MonoBehaviour
                 case 4:
                     Instantiate(greenGemStone, gemStoneSpawnArea.transform.position, Quaternion.identity);
                     break;
+
             }
         }
     }
@@ -69,7 +73,78 @@ public class forestMining : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (time.hour == 0)
+        {
+            if (spawn)
+            {
+                spawnRock();
+                spawn = false;
+            }
+        }
+        if (time.hour == 1)
+        {
+            spawn = true;
+        }
+        if (time.hour == 4)
+        {
+            if (spawn)
+            {
+                spawnRock();
+                spawn = false;
+            }
+        }
+        if (time.hour == 5)
+        {
+            spawn = true;
+        }
+        if (time.hour == 8)
+        {
+            if (spawn)
+            {
+                spawnRock();
+                spawn = false;
+            }
+        }
+        if (time.hour == 9)
+        {
+            spawn = true;
+        }
+        if (time.hour == 12)
+        {
+            if (spawn)
+            {
+                spawnRock();
+                spawn = false;
+            }
+        }
+        if (time.hour == 13)
+        {
+            spawn = true;
+        }
+        if (time.hour == 16)
+        {
+            if (spawn)
+            {
+                spawnRock();
+                spawn = false;
+            }
+        }
+        if (time.hour == 17)
+        {
+            spawn = true;
+        }
+        if (time.hour == 20)
+        {
+            if (spawn)
+            {
+                spawnRock();
+                spawn = false;
+            }
+        }
+        if (time.hour == 21)
+        {
+            spawn = true;
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -81,7 +156,7 @@ public class forestMining : MonoBehaviour
             {
                 GameController.changeBGS(dunSound, audioSource);
                 GameController.changeBGM(dunMusic, musicSource);
-               
+
                 player.transform.position = new Vector3(120.71f, -20.62f, 0f);
                 GameController.currentMap = PlayerMap.forrestMiningArea;
             }
@@ -101,11 +176,67 @@ public class forestMining : MonoBehaviour
                 {
                     GameController.changeBGS(forestNightAudio, audioSource);
                 }
-               
+
                 player.transform.position = new Vector3(119.98f, -6.38f, 0f);
                 GameController.currentMap = PlayerMap.forrest;
             }
         }
     }
+    public void spawnRock()
+    {
 
+        GameObject[] gemStones = GameObject.FindGameObjectsWithTag("gemStone");
+
+        foreach (GameObject gemStone in gemStones)
+        {
+            Destroy(gemStone);
+        }
+
+        GameObject[] rocks = GameObject.FindGameObjectsWithTag("rock");
+
+        foreach (GameObject rock in rocks)
+        {
+            Destroy(rock);
+        }
+
+        gemStoneSpawnAreas = GameObject.FindGameObjectsWithTag("gemStoneSpawnArea");
+        rockSpawnAreas = GameObject.FindGameObjectsWithTag("rockSpawnArea");
+
+        foreach (GameObject rockSpawnArea in rockSpawnAreas)
+        {
+            int rand = Random.Range(0, 4);
+            switch (rand)
+            {
+                case 0:
+                    Instantiate(rock, rockSpawnArea.transform.position, Quaternion.identity);
+                    break;
+            }
+        }
+
+        foreach (GameObject gemStoneSpawnArea in gemStoneSpawnAreas)
+        {
+
+            int rand = Random.Range(0, 20);
+
+            switch (rand)
+            {
+                case 0:
+                    Instantiate(blueGemStone, gemStoneSpawnArea.transform.position, Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(redGemStone, gemStoneSpawnArea.transform.position, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(yellowGemStone, gemStoneSpawnArea.transform.position, Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(orangeGemStone, gemStoneSpawnArea.transform.position, Quaternion.identity);
+                    break;
+                case 4:
+                    Instantiate(greenGemStone, gemStoneSpawnArea.transform.position, Quaternion.identity);
+                    break;
+
+            }
+        }
+    }
 }
--- Assets/Script/forestTreesArea.cs
+++ Assets/Script/forestTreesArea.cs
@@ -17,27 +17,89 @@ public class forestTreesArea : MonoBehaviour
     public AudioClip forestMusic;
     public AudioClip forestNightAudio;
 
+    private bool spawn = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        treesSpawnArea = GameObject.FindGameObjectsWithTag("treeSpawnArea");
         
-        foreach (GameObject treeSpawnArea in treesSpawnArea)
-        {
-            int rand = Random.Range(0, 4);
-            switch (rand)
-            {
-                case 0:
-                    Instantiate(Tree, treeSpawnArea.transform.position, Quaternion.identity);
-                    break;
-            }
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (time.hour == 0)
+        {
+            if (spawn)
+            {
+                spawnTrees();
+                spawn = false;
+            }
+        }
+        if (time.hour == 1)
+        {
+            spawn = true;
+        }
+        if (time.hour == 4)
+        {
+            if (spawn)
+            {
+                spawnTrees();
+                spawn = false;
+            }
+        }
+        if (time.hour == 5)
+        {
+            spawn = true;
+        }
+        if (time.hour == 8)
+        {
+            if (spawn)
+            {
+                spawnTrees();
+                spawn = false;
+            }
+        }
+        if (time.hour == 9)
+        {
+            spawn = true;
+        }
+        if (time.hour == 12)
+        {
+            if (spawn)
+            {
+                spawnTrees();
+                spawn = false;
+            }
+        }
+        if (time.hour == 13)
+        {
+            spawn = true;
+        }
+        if (time.hour == 16)
+        {
+            if (spawn)
+            {
+                spawnTrees();
+                spawn = false;
+            }
+        }
+        if (time.hour == 17)
+        {
+            spawn = true;
+        }
+        if (time.hour == 20)
+        {
+            if (spawn)
+            {
+                spawnTrees();
+                spawn = false;
+            }
+        }
+        if (time.hour == 21)
+        {
+            spawn = true;
+        }
 
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -75,5 +137,26 @@ public class forestTreesArea : MonoBehaviour
             }
         }
     }
+    public void spawnTrees()
+    {
+        GameObject[] trees = GameObject.FindGameObjectsWithTag("tree");
+        
+        foreach(GameObject tree in trees)
+        {
+            Destroy(tree);
+        }
 
+        treesSpawnArea = GameObject.FindGameObjectsWithTag("treeSpawnArea");
+
+        foreach (GameObject treeSpawnArea in treesSpawnArea)
+        {
+            int rand = Random.Range(0, 4);
+            switch (rand)
+            {
+                case 0:
+                    Instantiate(Tree, treeSpawnArea.transform.position, Quaternion.identity);
+                    break;
+            }
+        }
+    }
 }
